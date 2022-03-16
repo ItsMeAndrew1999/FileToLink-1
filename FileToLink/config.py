47401a755d2dcf71a7238f87ed7bcc242af45c69
ItsMeAndrew1999 <101704138+ItsMeAndrew1999@users.noreply.github.com>
2022-03-16 15:43:16 +0530

    Update config.py

--- FileToLink/config.py
+++ FileToLink/config.py
@@ -20,7 +20,6 @@ class Config:
 
     Link_Root = f"https://{App_Name}.herokuapp.com/"
     Download_Folder = "Files"
-    Dev_Channel = "shadow_bots"
     Bot_UserName = None  # The bot will set it after starting
     Part_size = 10 * 1024 * 1024  # (10MB) For Pyrogram
     Buffer_Size = 512 * 1024  # For Quart
@@ -35,8 +34,6 @@ class Strings:
     dl_link = "🔗 Download LINK"
     st_link = "🎞 Stream LINK"
     generating_link = "**⏳ Generating Link...**"
-    bot_channel = "📢 Bot Channel"
-    dev_channel = "🤖 Developer"
     fast = "⚡️**The link has been updated to a fast link**"
     update_link = "⚡ Update To Fast Link"
     update_limited = (f"⛔ You can update just {Config.Max_Fast_Processes} link in one time, "
